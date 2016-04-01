a7135ee9c619e4a7f67231513015d7240a019648
Jesse Houwing <jesse.houwing@gmail.com>
2016-04-01 19:36:35 +0200

    Adds pre-build task to copy shared file to all tasks automatically

---
+++ Pre-Build.ps1
@@ -0,0 +1,4 @@
+Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-delete -force
+Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-add -force
+Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-undo -force
+Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-checkin -force
