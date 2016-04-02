6fc77d4dea3b9ae862b929d3320464aee7101d8a
Jesse Houwing <jesse.houwing@gmail.com>
2016-04-02 21:15:38 +0200

    Feature/updateshelveset
    * First implementation of Update / Create shelveset
    
    * Copy shared files to update shelveset task
    
    * fix the extension manifest
    
    * Improved solution for default shelveset variable.
    
    * Enable update gated changes.
    
    * Enable update gated changes.
    
    * Improved efficiency by only gathering local changes in case this is a gated build.
    
    * Further performance improvements

--- Pre-Build.ps1
+++ Pre-Build.ps1
@@ -2,3 +2,4 @@ Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-delete -force
 Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-add -force
 Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-undo -force
 Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-checkin -force
+Copy-Item vsts-tfvc-shared\*.psm1 vsts-tfvc-updateshelveset -force
