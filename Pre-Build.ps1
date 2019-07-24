832947987ede78b9b62032c79b786998e2ddbb83
Jesse Houwing <jesse.houwing@gmail.com>
2019-07-24 17:54:56 +0200

    Fix build script

--- Pre-Build.ps1
+++ Pre-Build.ps1
@@ -1,6 +1,6 @@
-Copy-Item vsts-tfvc-shared\* vsts-tfvc-delete -force -recurse -exclude *.*proj
-Copy-Item vsts-tfvc-shared\* vsts-tfvc-add -force -recurse -exclude *.*proj
-Copy-Item vsts-tfvc-shared\* vsts-tfvc-undo -force -recurse -exclude *.*proj
-Copy-Item vsts-tfvc-shared\* vsts-tfvc-checkin -force -recurse -exclude *.*proj
-Copy-Item vsts-tfvc-shared\* vsts-tfvc-checkout -force -recurse -exclude *.*proj
-Copy-Item vsts-tfvc-shared\* vsts-tfvc-updateshelveset -force -recurse -exclude *.*proj
+Copy-Item tfvc-shared\* tfvc-delete -force -recurse -exclude *.*proj
+Copy-Item tfvc-shared\* tfvc-add -force -recurse -exclude *.*proj
+Copy-Item tfvc-shared\* tfvc-undo -force -recurse -exclude *.*proj
+Copy-Item tfvc-shared\* tfvc-checkin -force -recurse -exclude *.*proj
+Copy-Item tfvc-shared\* tfvc-checkout -force -recurse -exclude *.*proj
+Copy-Item tfvc-shared\* tfvc-updateshelveset -force -recurse -exclude *.*proj
