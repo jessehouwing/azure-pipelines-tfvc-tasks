bd464edba44b8ac4717ac562ae56d854dcfd32c4
Jesse Houwing <jesse.houwing@gmail.com>
2019-07-29 16:09:12 +0200

    Move everything to v2 folder
    - move everything to v2
    - make preview
    - bump version numbers
    - update manifest
    - remove old ps handler
    - strip vs projs

--- Pre-Build.ps1
+++ Pre-Build.ps1
@@ -1,6 +1,13 @@
-Copy-Item tfvc-shared\* tfvc-delete -force -recurse -exclude *.*proj
-Copy-Item tfvc-shared\* tfvc-add -force -recurse -exclude *.*proj
-Copy-Item tfvc-shared\* tfvc-undo -force -recurse -exclude *.*proj
-Copy-Item tfvc-shared\* tfvc-checkin -force -recurse -exclude *.*proj
-Copy-Item tfvc-shared\* tfvc-checkout -force -recurse -exclude *.*proj
-Copy-Item tfvc-shared\* tfvc-updateshelveset -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v1\* tf-vc-delete\v1 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v1\* tf-vc-add\v1 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v1\* tf-vc-undo\v1 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v1\* tf-vc-checkin\v1 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v1\* tf-vc-checkout\v1 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v1\* tf-vc-shelveset-update\v1 -force -recurse -exclude *.*proj
+
+Copy-Item tf-vc-shared\v2\* tf-vc-delete\v2 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v2\* tf-vc-add\v2 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v2\* tf-vc-undo\v2 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v2\* tf-vc-checkin\v2 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v2\* tf-vc-checkout\v2 -force -recurse -exclude *.*proj
+Copy-Item tf-vc-shared\v2\* tf-vc-shelveset-update\v2 -force -recurse -exclude *.*proj
