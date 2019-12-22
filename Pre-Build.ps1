7fb68d30470d8d5d3a3f9773a163f33408a160bc
Jesse Houwing <jesse.houwing@gmail.com>
2019-12-22 23:06:01 +0100

    Adds TlsHelper from Azure-Pipelines-Tasks to fix TLS 1.2 issues
    While this should really have been a concern of the PowerShell Execution Handler or the Azure-Pipelines-Task-Lib, it seems that a fix is needed until this is ported to where it belongs.

--- Pre-Build.ps1
+++ Pre-Build.ps1
@@ -1,13 +1,13 @@
-Copy-Item tf-vc-shared\v1\* tf-vc-delete\v1 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v1\* tf-vc-add\v1 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v1\* tf-vc-undo\v1 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v1\* tf-vc-checkin\v1 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v1\* tf-vc-checkout\v1 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v1\* tf-vc-shelveset-update\v1 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v1\* $PSScriptRoot\tf-vc-delete\v1 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v1\* $PSScriptRoot\tf-vc-add\v1 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v1\* $PSScriptRoot\tf-vc-undo\v1 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v1\* $PSScriptRoot\tf-vc-checkin\v1 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v1\* $PSScriptRoot\tf-vc-checkout\v1 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v1\* $PSScriptRoot\tf-vc-shelveset-update\v1 -force -recurse -exclude *.*proj
 
-Copy-Item tf-vc-shared\v2\* tf-vc-delete\v2 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v2\* tf-vc-add\v2 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v2\* tf-vc-undo\v2 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v2\* tf-vc-checkin\v2 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v2\* tf-vc-checkout\v2 -force -recurse -exclude *.*proj
-Copy-Item tf-vc-shared\v2\* tf-vc-shelveset-update\v2 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v2\* $PSScriptRoot\tf-vc-delete\v2 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v2\* $PSScriptRoot\tf-vc-add\v2 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v2\* $PSScriptRoot\tf-vc-undo\v2 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v2\* $PSScriptRoot\tf-vc-checkin\v2 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v2\* $PSScriptRoot\tf-vc-checkout\v2 -force -recurse -exclude *.*proj
+Copy-Item $PSScriptRoot\tf-vc-shared\v2\* $PSScriptRoot\tf-vc-shelveset-update\v2 -force -recurse -exclude *.*proj
