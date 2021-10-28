aa1d7a6e1f524171c6d2516d44dd4118e5498c0d
Jesse Houwing <jesse.houwing@gmail.com>
2021-10-28 13:07:14 +0200

    Disable defaullt config changes

--- tf-vc-fixparallel/v2/TfvcFixParallel.v3.ps1
+++ tf-vc-fixparallel/v2/TfvcFixParallel.v3.ps1
@@ -13,7 +13,7 @@ $teamProject = Get-VstsTaskVariable -Name "System.TeamProject" -Require
 
 #& az config set extension.use_dynamic_install=yes_without_prompt
 #& az extension add --name azure-devops
-& az devops configure --defaults organization=$org project=$teamProject
+#& az devops configure --defaults organization=$org project=$teamProject
 $vssCredential | &  az devops login --org $org
 
 # Create header with PAT
